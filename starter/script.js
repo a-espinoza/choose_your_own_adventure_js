e6e84c34a3fc16c0f9c9733af812ac480f07b255
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-09-20 02:41:26 -0400

    better late than never

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,34 @@
-// put code here!
+//made this in my codeacademy account(not original)
+
+confirm("are you ready to shake and bake");
+
+var age = prompt("what's your age");
+
+if(age < 13){
+    console.log ("hey, you are allowed to play but I take no responsibilit")
+    }
+else{
+    console.log("anwesome, lets get started")
+};
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log ("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+
+if (userAnswer == "yes"){
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!")
+}
+else{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'")
+    }
+
+var feedback = prompt ("Rate this game,1-10, 10 being ANWESOME")
+
+if(feedback>8){
+    console.log("Thank you! We should race at the next concert!")
+    }
+    else{
+        console.log("I'll keep practicing coding and racing.")
+        }
